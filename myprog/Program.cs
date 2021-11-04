cad78b7d14e344126e2844c383973889b3818f53
niranda <teslatorgames@gmail.com>
2021-11-04 14:35:52 +0200

    added default project

---
+++ myprog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myprog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
